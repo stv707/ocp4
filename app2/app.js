d9d7d1a20cbda4927c039a7ca56ecfd82d4561ed
Student User <student@workstation.lab.example.com>
2021-10-27 08:31:05 -0400

    v3

---
+++ app2/app.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const os = require('os');
+
+const yaml = require('js-yaml');
+
+const fs   = require('fs');
+
+const doc1 = yaml.load(fs.readFileSync('./data/app.yml', 'utf8'));
+
+
+function getIPAddress() {
+  var interfaces = require('os').networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+        return alias.address;
+    }
+  }
+  return '0.0.0.0';
+}
+
+console.log("AppRed server starting...");
+
+var handler = function(request, response) {
+  console.log("Received request from " + request.connection.remoteAddress);
+  response.writeHead(200);
+  response.end("PodName " + os.hostname() + " running on ip:" +  getIPAddress()  + " DATA:" +   JSON.stringify(doc1)  + "\n");
+  
+};
+var www = http.createServer(handler);
+www.listen(8080);
+
+
+
